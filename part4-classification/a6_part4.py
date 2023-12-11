198e3a6ecb9dfa0c94978a28e4b93a1ec81e1884
Neel Nadkarni <nsnadkarni@cps.edu>
2023-12-11 00:04:44 -0600

    done

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -4,24 +4,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("part4-classification/suv_data.csv")
-data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
+data['Gender'].replace(['Male', 'Female'], [0, 1], inplace=True)
 
 x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
-# Step 1: Print the values for x and y
+print("Values of x:")
+print(x)  
+print("\nValues of y:")
+print(y)  
 
-# Step 2: Standardize the data using StandardScaler, 
+scaler = StandardScaler()
 
-# Step 3: Transform the data
+x_scaled = scaler.fit_transform(x)
 
-# Step 4: Split the data into training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2)
 
-# Step 5: Fit the data
+logistic_model = linear_model.LogisticRegression()
+logistic_model.fit(x_train, y_train)
 
-# Step 6: Create a LogsiticRegression object and fit the data
+accuracy = logistic_model.score(x_test, y_test)
+print("\nAccuracy of the model:", accuracy)
 
-# Step 7: Print the score to see the accuracy of the model
+y_pred = logistic_model.predict(x_test)
 
-# Step 8: Print out the actual ytest values and predicted y values
-# based on the xtest data
+print("\nActual ytest values:")
+print(y_test)  
+
+print("\nPredicted y values:")
+print(y_pred)  
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -6,9 +6,17 @@ After completing `a6_part4.py` answer the following questions
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
 
+0.925 with Scaler 0.6875 without scalar. It is less accurate because some features of the data can be stronger than others without scalar.
+
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
+
+0.925, it is definetly accurate enough as it is very close to 1.
 
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
 
+The model did really well. There wasn't really a pattern.
+
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
+
+No
 
